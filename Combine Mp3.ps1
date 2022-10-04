613ef158dc3274bb80ba6bc1e4c076b63b8c3916
Leafeon182 <113450405+Leafeon182@users.noreply.github.com>
2022-10-04 16:01:07 -0400

    Add files via upload
    Script that I setup to combine audio files into large, combined file. Note that ffmpeg is a pre-requisite for this and will not function properly unless the ffmpeg executable is in the script directory.

---
+++ Combine Mp3.ps1
@@ -0,0 +1,28 @@
+
+#Asks the user for the file name, since a bunch of files might be combined, it may be best to just ask what the ending file name ought to be.
+$filename = Read-Host "Enter the Filename you'd wish to name your output file. The file extension will be automatically set to .mp3"
+#creates the output folder, this is needed to drop the end result file in.
+mkdir .\output -force
+#removes the mylist.txt file if it already exists.
+#This is for cleanliness, otherwise an old file be written over, appended, etc, I think its best to just start fresh.
+Remove-Item .\mylist.txt -force
+#writes the "mylist.txt file"
+#this is required by the ffmpeg application.
+#FFMPEG concatenate requires the list of files you wish to combine to be in a specific format
+#That format is literally - file 'FULL\FILE\PATH\TO\MEDIA\FILE.MP3'
+#for example:
+#file 'X:\Mux\Lolita\01-01 1a.mp3'
+#file 'X:\Mux\Lolita\01-01 1b.mp3'
+#file 'X:\Mux\Lolita\01-01 1c.mp3'
+foreach ($i in Get-ChildItem .\*.mp3) {echo "file '$i'" >> mylist.txt}
+#FFMPEG concat requires that the input file be in UTF8 ONLY. Otherwise it won't work.
+#This grabs the file, assigns it to a variable, then re-writes it over itself as a utf-8 file.
+#note that using flag -encoding utf8 results in the file being written as UTF-8 with BOM. So, apparently, -encoding ASCII is what we want. 
+$Files = (Get-Content .\mylist.txt)
+$Files | Out-File -Encoding "ASCII" .\mylist.txt
+
+#calls the ffmpeg executable from the current directory. issues the concatenate command, feeds it the mylist.txt file.
+#then, writes the output using the file name defined by the user in line #1
+.\ffmpeg -f concat -safe 0 -i .\mylist.txt -c copy ".\Output\$filename.mp3"
+
+pause
